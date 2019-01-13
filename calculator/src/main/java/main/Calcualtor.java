24f97c1986e819a916e97c3bd07047401f5a5016
Piotr Stawirej <piotr.stawirej@gmail.com>
2019-01-13 18:37:58 +0100

    working

--- calculator/src/main/java/main/Calcualtor.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public final class Calcualtor {
-
-    public static long add(int a, int b) {
-        return a + b;
-    }
-}
--- calculator/src/test/java/test/CaclulatorScenarios.java
+++ calculator/src/test/java/test/CaclulatorScenarios.java
@@ -1,19 +1,5 @@
 package test;
 
-import static org.assertj.core.api.BDDAssertions.then;
-
-import main.Calcualtor;
-import org.junit.jupiter.api.Test;
-
 final class CaclulatorScenarios {
-
-    @Test
-    void add() {
-        // When
-        var sum = Calcualtor.add(1, 2);
-
-        // Then
-        then(sum).isEqualTo(3);
-    }
 
 }
