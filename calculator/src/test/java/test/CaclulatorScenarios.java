c4d148dc8d4fbc2d27ec65b42406170522703ef6
Piotr Stawirej <piotr.stawirej@gmail.com>
2019-01-13 18:51:34 +0100

    testing custom message

--- calculator/src/test/java/test/CaclulatorScenarios.java
+++ calculator/src/test/java/test/CaclulatorScenarios.java
@@ -1,5 +1,13 @@
 package test;
 
+import static org.assertj.core.api.BDDAssertions.then;
+
+import org.junit.jupiter.api.Test;
+
 final class CaclulatorScenarios {
 
+    @Test
+    void foo() {
+        then(Boolean.TRUE).isTrue();
+    }
 }
