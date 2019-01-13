d6174f186e7f6cfe773df47580fa085a4bc02ece
Piotr Stawirej <piotr.stawirej@gmail.com>
2019-01-13 15:16:54 +0100

    Initial calculator commit.

---
+++ calculator/src/test/java/test/CaclulatorScenarios.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+import org.junit.jupiter.api.Test;
+
+final class CaclulatorScenarios {
+
+    @Test
+    void foo() {
+        then(Boolean.TRUE).isTrue();
+    }
+
+}
