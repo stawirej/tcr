481826c5ef9a47110d83cf3b944ef9afc954c6b1
Piotr Stawirej <piotr.stawirej@gmail.com>
2019-03-11 23:21:52 +0100

    Add TCR scripts.

---
+++ calculator/tcr.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+test() {
+    ./test.sh | tee ./tcrfeedback 2>&1
+}
+
+commit() {
+    git add .
+    if [[ $# -eq 0 ]]
+            then git commit -am working
+            else git commit -am "$1"
+    fi
+}
+
+test_code_only() {
+    git status | grep src
+}
+
+revert() {
+    git reset --hard
+    git clean -fd
+}
+
+test && commit "$@" || revert
+#(test && commit "$@") || test_code_only || revert
