f0eaababc614187953f3d59114cb8e6aa9ff8bf9
Piotr Stawirej <piotr.stawirej@gmail.com>
2019-04-08 00:00:57 +0200

    working

--- calculator/src/test/java/calculator/Calcualtor.java
+++
@@ -1,8 +0,0 @@
-package calculator;
-
-final class Calcualtor {
-
-    public static int sum(int a, int b) {
-        return 3;
-    }
-}
--- calculator/src/test/java/calculator/CalculatorScenarios.java
+++ calculator/src/test/java/calculator/CalculatorScenarios.java
@@ -6,15 +6,5 @@ import org.junit.jupiter.api.Test;
 
 final class CalculatorScenarios {
 
-    @Test
-    void addNumbers(){
-        // Given
-
-        // When
-        int sum = Calcualtor.sum(1, 2);
-
-        // Then
-        then(sum).isEqualTo(3);
-    }
 }
 
