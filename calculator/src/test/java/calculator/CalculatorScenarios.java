c17e4e90b304d6884afe0d979aed48b9e2354225
Piotr Stawirej <piotr.stawirej@gmail.com>
2019-04-08 16:23:15 +0200

    working

--- calculator/src/test/java/calculator/CalculatorScenarios.java
+++ calculator/src/test/java/calculator/CalculatorScenarios.java
@@ -1,6 +1,20 @@
 package calculator;
 
+import static org.assertj.core.api.BDDAssertions.then;
+
+import org.junit.jupiter.api.Test;
+
 final class CalculatorScenarios {
 
+    @Test
+    void addNumbers(){
+        // Given
+
+        // When
+        int sum = Calculator.sum(1,2 );
+
+        // Then
+        then(sum).isEqualTo(3);
+    }
 }
 
