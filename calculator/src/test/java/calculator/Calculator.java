d9b1d1bdd5bbf43e33a196a69c51f2013eb94d3d
Piotr Stawirej <piotr.stawirej@gmail.com>
2019-04-08 22:27:13 +0200

    Revert to raw project.

--- calculator/src/test/java/calculator/Calculator.java
+++
@@ -1,8 +0,0 @@
-package calculator;
-
-final class Calculator {
-
-    public static int sum(int a, int b) {
-        return 3;
-    }
-}
--- calculator/src/test/java/calculator/CalculatorScenarios.java
+++ calculator/src/test/java/calculator/CalculatorScenarios.java
@@ -1,20 +1,6 @@
 package calculator;
 
-import static org.assertj.core.api.BDDAssertions.then;
-
-import org.junit.jupiter.api.Test;
-
 final class CalculatorScenarios {
 
-    @Test
-    void addNumbers(){
-        // Given
-
-        // When
-        int sum = Calculator.sum(1,2 );
-
-        // Then
-        then(sum).isEqualTo(3);
-    }
 }
 
