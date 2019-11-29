865db24f7fbc76f82a27a5ae2680f90c77cee1ed
Piotr Stawirej <piotr.stawirej@gmail.com>
2019-11-29 21:34:15 +0100

    working

--- calculator/src/test/java/calculator/CalculatorScenarios.java
+++ calculator/src/test/java/calculator/CalculatorScenarios.java
@@ -1,11 +1,7 @@
 package calculator;
 
-import static org.assertj.core.api.Java6BDDAssertions.then;
-
-import org.junit.jupiter.api.Test;
-
 final class CalculatorScenarios {
 
-    int x = 1;
+    
 }
 
